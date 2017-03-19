da699019cfe1013a6a9d1b5a89db857df78ca6d0
Shabla <vitaligenius99@gmail.com>
2017-03-19 16:52:13 +0300

    delete all windows dep.

--- ECS/include/ECS/ECS.h
+++ ECS/include/ECS/ECS.h
@@ -1,9 +1,18 @@
 #pragma once
 
-#ifdef ECS_EXPORTS
-#define ECS_API __declspec(dllexport) 
+#if defined(_MSC_VER)
+	//  Microsoft 
+	#define EXPORT __declspec(dllexport)
+	#define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+	//  GCC
+	#define EXPORT __attribute__((visibility("default")))
+	#define IMPORT
 #else
-#define ECS_API __declspec(dllimport) 
+	//  do nothing and hope for the best?
+	#define EXPORT
+	#define IMPORT
+	#pragma warning Unknown dynamic link import/export semantics.
 #endif
 
 namespace ECS
